da1749a206e7df0b239402b2f9f8aa51b14df0c2
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-12-26 09:20:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace karakterdegistirme
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Aralarında Boşluk Bırakarak kelimeler Giriniz:");
+            string Kelimeler = Console.ReadLine();
+            string[] kelime = Kelimeler.Split();
+
+            foreach (var item in kelime)
+            {
+
+                char[] karakter = item.ToCharArray();
+                char temp = karakter[0];
+
+                karakter[0] = karakter[karakter.Length - 1];
+                karakter[karakter.Length - 1] = temp;
+
+                foreach (var item1 in karakter)
+                {
+                    string yeni=item1.ToString();
+                    Console.Write(yeni);
+                }
+            }
+        }
+    }
+}
